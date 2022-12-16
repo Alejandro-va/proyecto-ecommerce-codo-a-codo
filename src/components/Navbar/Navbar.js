713820bc029f726dc74cb65c6b46a21fb04e6e39
Alejandro-va <prof.jahz@gmail.com>
2022-12-15 21:10:45 -0300

    iconos nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ import "./styles.css";
 const Navbar = () => {
   const [mode, setMode] = useState("bedtime");
   const [respMenu, setRespMenu] = useState(false);
-  const { loggedUser } = useContext(SessionContext)
+  const { loggedUser } = useContext(SessionContext);
   const { cart } = useContext(ProductContext);
 
   let setModeSite = () => {
@@ -25,15 +25,15 @@ const Navbar = () => {
   const getCartItemCount = () => {
     let count = 0;
     if (cart.length > 0) {
-      cart.forEach(item => count += item.quantity)
+      cart.forEach((item) => (count += item.quantity));
     }
     return count;
-  }
+  };
 
   return (
     <header className="header">
       <nav className="navbar">
-        <Link to='/products'>
+        <Link to="/products">
           <div className="logo">
             <img
               src="https://github.com/subeshb1/GrabCheap/blob/master/img/logo_inverse.jpg?raw=true"
@@ -42,41 +42,42 @@ const Navbar = () => {
             <div className="text"> Ecommerce </div>
           </div>
         </Link>
-
-        <div className="right full-screen">
-          <div className="group darkmode" onClick={() => setModeSite()}>
-            <div className="mode">
-              <i className="material-icons" onClick={() => setModeSite}>
-                {mode}
-              </i>
+        <div class="grupo-icono">
+          <div className="right full-screen">
+            <div className="group darkmode" onClick={() => setModeSite()}>
+              <div className="mode">
+                <i className="material-icons" onClick={() => setModeSite}>
+                  {mode}
+                </i>
+              </div>
             </div>
-          </div>
 
-          <div className="group">
-            <i className="material-icons">account_circle</i>
-            {!loggedUser ? (
-              <a href="/login" className="item">
-                <span className="detail">Sign In</span>
-              </a>
-            ) : (
-              <a href="/profile" className="item">
-                <span className="detail">{loggedUser.email}</span>
-              </a>
-            )}
-          </div>
+            <div className="group">
+              <i className="material-icons">account_circle</i>
+              {!loggedUser ? (
+                <a href="/login" className="item">
+                  <span className="detail">Sign In</span>
+                </a>
+              ) : (
+                <a href="/profile" className="item">
+                  <span className="detail">{loggedUser.email}</span>
+                </a>
+              )}
+            </div>
 
-          <div className="group cart">
-            <i className="material-icons">shopping_cart</i>
-            <a href="/cart" className="item">
-              <span className="detail">Cart</span>
-              <span className="cart-items-count">{getCartItemCount()}</span>
-            </a>
-          </div>
-          <div className="group contact">
-            <i className="material-icons">call</i>
-            <a href="/contact" className="item">
-              <span className="detail">Contacto</span>
-            </a>
+            <div className="group cart">
+              <i className="material-icons">shopping_cart</i>
+              <a href="/cart" className="item">
+                <span className="detail">Cart</span>
+                <span className="cart-items-count">{getCartItemCount()}</span>
+              </a>
+            </div>
+            <div className="group contact">
+              <i className="material-icons">call</i>
+              <a href="/contact" className="item">
+                <span className="detail">Contacto</span>
+              </a>
+            </div>
           </div>
         </div>
         <div className="group menu" onClick={() => setRespMenu(true)}>
